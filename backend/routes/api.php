50f7ad0b75507659e5cc66f25028159a56670284
Quang <quang.nm1@zinza.com.vn>
2024-10-08 10:31:50 +0700

    fix: url api article

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\Api\ArticleController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/article', [ArticleController::class, 'index']);
-Route::post('/article', [ArticleController::class, 'store']);
-Route::get('/article/{slug}', [ArticleController::class, 'show']);
-Route::put('/article/{slug}', [ArticleController::class, 'update']);
-Route::delete('/article/{slug}', [ArticleController::class, 'destroy']);
+Route::get('/articles', [ArticleController::class, 'index']);
+Route::post('/articles', [ArticleController::class, 'store']);
+Route::get('/articles/{slug}', [ArticleController::class, 'show']);
+Route::put('/articles/{slug}', [ArticleController::class, 'update']);
+Route::delete('/articles/{slug}', [ArticleController::class, 'destroy']);
