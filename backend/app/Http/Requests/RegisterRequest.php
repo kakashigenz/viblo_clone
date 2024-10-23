3634aa0e75558b8635d1b66d66f84e76be28b469
Quang <quang.nm1@zinza.com.vn>
2024-10-23 10:23:01 +0700

    fix: validate register

--- backend/app/Http/Requests/RegisterRequest.php
+++ backend/app/Http/Requests/RegisterRequest.php
@@ -16,7 +16,7 @@ class RegisterRequest extends FormRequest
         return [
             'name' => 'required',
             'email' => 'required|unique:users,email',
-            'user_name' => 'required|unique:users,user_name',
+            'user_name' => 'required|unique:users,user_name|regex:/^[A-Za-z0-9-]+$/',
             'password' => 'required|regex:/^\w{8,}$/|confirmed',
         ];
     }
