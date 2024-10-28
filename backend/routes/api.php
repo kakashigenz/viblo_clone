29f92fac7c8c4fc8f1c4f50c51a6a232224ad470
Quang <quang.nm1@zinza.com.vn>
2024-10-28 15:07:00 +0700

    refactor: rewite prefix route group

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,47 +16,47 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
-    Route::group(['as' => 'article.'], function () {
-        Route::get('/articles', [ArticleController::class, 'index']);
-        Route::post('/articles', [ArticleController::class, 'store']);
-        Route::get('/articles/{slug}', [ArticleController::class, 'show']);
-        Route::put('/articles/{slug}', [ArticleController::class, 'update']);
-        Route::delete('/articles/{slug}', [ArticleController::class, 'destroy']);
-        Route::post('/articles/{article_slug}/bookmark', [BookmarkController::class, 'store']);
-        Route::delete('/articles/{article_slug}/unbookmark', [BookmarkController::class, 'destroy']);
-        Route::post('/articles/{article_slug}/upvote', [VoteController::class, 'upvote'])->name('upvote');
-        Route::post('/articles/{article_slug}/downvote', [VoteController::class, 'downvote'])->name('downvote');
+    Route::group(['as' => 'article.', 'prefix' => 'articles'], function () {
+        Route::get('/', [ArticleController::class, 'index']);
+        Route::post('/', [ArticleController::class, 'store']);
+        Route::post('/{article_slug}/bookmark', [BookmarkController::class, 'store']);
+        Route::delete('/{article_slug}/unbookmark', [BookmarkController::class, 'destroy']);
+        Route::post('/{article_slug}/upvote', [VoteController::class, 'upvote'])->name('upvote');
+        Route::post('/{article_slug}/downvote', [VoteController::class, 'downvote'])->name('downvote');
+        Route::get('/{slug}/comments', [CommentController::class, 'index']);
+        Route::post('/{slug}/comments', [CommentController::class, 'store']);
+        Route::get('/{slug}', [ArticleController::class, 'show']);
+        Route::put('/{slug}', [ArticleController::class, 'update']);
+        Route::delete('/{slug}', [ArticleController::class, 'destroy']);
     });
 
-    Route::group(['as.' => 'tag.'], function () {
-        Route::get('/tags', [TagController::class, 'index']);
-        Route::post('/tags', [TagController::class, 'store']);
-        Route::get('/tags/{slug}', [TagController::class, 'show']);
-        Route::delete('/tags/{slug}', [TagController::class, 'destroy']);
-        Route::post('tags/{tag_slug}/follow', [FollowingTagController::class, 'follow']);
-        Route::delete('tags/{tag_slug}/unfollow', [FollowingTagController::class, 'unfollow']);
+    Route::group(['as.' => 'tag.', 'prefix' => 'tags'], function () {
+        Route::get('/', [TagController::class, 'index']);
+        Route::post('/', [TagController::class, 'store']);
+        Route::get('/{slug}', [TagController::class, 'show']);
+        Route::delete('/{slug}', [TagController::class, 'destroy']);
+        Route::post('/{tag_slug}/follow', [FollowingTagController::class, 'follow']);
+        Route::delete('/{tag_slug}/unfollow', [FollowingTagController::class, 'unfollow']);
     });
 
-    Route::group(['as' => 'image.'], function () {
-        Route::post('/images', [ImageController::class, 'createPresignedURL']);
-        Route::get('/images', [ImageController::class, 'index']);
-        Route::delete('/images/{name}', [ImageController::class, 'destroy']);
+    Route::group(['as' => 'image.', 'prefix' => 'images'], function () {
+        Route::post('/', [ImageController::class, 'createPresignedURL']);
+        Route::get('/', [ImageController::class, 'index']);
+        Route::delete('/{name}', [ImageController::class, 'destroy']);
     });
 
-    Route::group(['as' => 'user.'], function () {
-        Route::get('users/me', [UserController::class, 'getCurrentUser']);
-        Route::get('users/{user_name}', [UserController::class, 'show']);
-        Route::put('users/{user_name}', [UserController::class, 'update']);
-        Route::get('users/{user_name}/followings', [FollowingUserController::class, 'index']);
-        Route::post('users/{user_name}/follow', [FollowingUserController::class, 'follow']);
-        Route::delete('users/{user_name}/unfollow', [FollowingUserController::class, 'unfollow']);
-        Route::get('/users/{user_name}/following-tags', [FollowingTagController::class, 'index']);
-        Route::get('/users/{user_name}/bookmarks', [BookmarkController::class, 'index']);
+    Route::group(['as' => 'user.', 'prefix' => 'users'], function () {
+        Route::get('/me', [UserController::class, 'getCurrentUser']);
+        Route::get('/{user_name}', [UserController::class, 'show']);
+        Route::put('/{user_name}', [UserController::class, 'update']);
+        Route::get('/{user_name}/followings', [FollowingUserController::class, 'index']);
+        Route::post('/{user_name}/follow', [FollowingUserController::class, 'follow']);
+        Route::delete('/{user_name}/unfollow', [FollowingUserController::class, 'unfollow']);
+        Route::get('/{user_name}/following-tags', [FollowingTagController::class, 'index']);
+        Route::get('/{user_name}/bookmarks', [BookmarkController::class, 'index']);
     });
 
-    Route::group(['as' => 'comment.'], function () {
-        Route::get('/article/{slug}/comments', [CommentController::class, 'index']);
-        Route::post('/article/{slug}/comments', [CommentController::class, 'store']);
+    Route::group(['as' => 'comment.', 'prefix' => 'comments'], function () {
         Route::put('/comments/{id}', [CommentController::class, 'update']);
         Route::delete('/comments/{id}', [CommentController::class, 'destroy']);
         Route::get('/comments/{comment_id}/replies', [CommentController::class, 'getSubComments']);
