1010ee97e1e1236a23023c63789806d5c1d281c0
Quang <quang.nm1@zinza.com.vn>
2024-11-22 15:11:08 +0700

    feat: make banner and navigation bar component

---
+++ frontend/src/components/Banner.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex justify-center items-center" style="background-color: #1481c5">
+    <div class="max-w-[1140px] max-h-[168px]">
+      <img src="http://images.viblo.test/images/banner.png" alt="banner" class="" />
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ frontend/src/components/NavigationBar.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="bg-[#0b1a33] py-4">
+    <Container class="flex items-center">
+      <ul class="flex items-center gap-x-[40px] px-8">
+        <li v-for="option in navOptions">
+          <RouterLink
+            v-if="option.route"
+            :to="{ name: option.route }"
+            class="block text-white font-bold text-xs text-center uppercase nav-link"
+            >{{ option.title }}</RouterLink
+          >
+          <a
+            v-else
+            href="#"
+            class="block text-white font-bold text-xs uppercase nav-link"
+            >{{ option.title }}</a
+          >
+        </li>
+      </ul>
+      <div>
+        <a
+          href="#"
+          class="bg-sky-600 text-white rounded-sm px-4 py-1 flex items-center gap-x-2"
+        >
+          <i class="pi pi-pencil" style="font-size: 14px"></i>
+          <span> Viết bài </span>
+        </a>
+      </div>
+    </Container>
+  </div>
+</template>
+
+<script setup>
+import {
+  BOOKMARKS_ROUTE_NAME,
+  FOLLOWINGS_ROUTE_NAME,
+  NEWEST_ROUTE_NAME,
+} from "@/helper/constant";
+
+import Container from "./Container.vue";
+
+const navOptions = [
+  {
+    title: "Nhà sáng tạo nội dung",
+  },
+  {
+    title: "Đang theo dõi",
+    route: FOLLOWINGS_ROUTE_NAME,
+  },
+  {
+    title: "Mới nhất",
+    route: NEWEST_ROUTE_NAME,
+  },
+  {
+    title: "Series",
+  },
+
+  {
+    title: "Editor choice",
+  },
+  {
+    title: "Trending",
+  },
+  {
+    title: "Video",
+  },
+  {
+    title: "Bookmark của tôi",
+    route: BOOKMARKS_ROUTE_NAME,
+  },
+];
+</script>
+
+<style lang="css" scoped>
+.nav-link::after {
+  content: "";
+  display: block;
+  width: 0;
+  height: 2px;
+  background-color: #fff;
+  transition: all ease 0.4s;
+  margin-top: 6px;
+}
+
+.nav-link:hover.nav-link::after {
+  width: 100%;
+}
+
+.router-link-active::after {
+  width: 100%;
+}
+</style>
