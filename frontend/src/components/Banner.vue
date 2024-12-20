36658b9e6a171c453ab1cf58d03fa2e847085663
quang <quang.nm1@zinza.com.vn>
2024-12-20 09:17:11 +0700

    fix: change banner and background color

--- frontend/src/components/Banner.vue
+++ frontend/src/components/Banner.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="flex justify-center items-center" style="background-color: #1481c5">
+  <div
+    class="flex justify-center items-center"
+    style="background-color: rgb(51, 94, 222)"
+  >
     <div class="max-w-[1140px] max-h-[168px]">
-      <img src="http://images.viblo.test/images/banner.png" alt="banner" class="" />
+      <img src="/images/banner.jpg" alt="banner" class="" />
     </div>
   </div>
 </template>
